81f7efeafab3554401fe4ff69528dbfedf2e85c1
lulu0705 <86127529+lulu0705@users.noreply.github.com>
2022-12-28 02:00:52 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+// import { CookiesProvider } from 'react-cookie';
+import { Provider } from "react-redux";
+import store from './redux/store';
+
+
+import './App.css';
+import Home from './pages/Home';
+import Product from './pages/Product';
+import ResultPage from './pages/ResultPage';
+import Feedback from './pages/Feedback';
+import Reserve from './pages/Reserve';
+
+import Index from './pages/Index';
+
+// import Indoor from './pages/Indoor';
+// import Blockage from './pages/Blockage';
+// import Bugs from './pages/Bugs';
+// import DoorWindowFix from './pages/DoorWindowFix';
+// import Storage from './pages/Storage';
+
+const queryClient = new QueryClient()
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Index" element={<Index />} />
+        <Route path="products/category/:categoryName" element={<Index />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/ResultPage" element={<ResultPage />} />
+        <Route path="/Feedback" element={<Feedback />} />
+        <Route path="/Reserve/:productId" element={<Reserve />} />
+      </Routes>    
+    </BrowserRouter>
+    </QueryClientProvider>
+
+    // <CookiesProvider>
+    // <QueryClientProvider client={queryClient}>
+    // <Provider store={store}>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/Index" element={<Index />} />
+    //     <Route path="products/category/:categoryName" element={<Index />} />
+    //     <Route path="/product/:productId" element={<Product />} />
+    //     <Route path="/ResultPage" element={<ResultPage />} />
+    //     <Route path="/Feedback" element={<Feedback />} />
+    //     <Route path="/Reserve/:productId" element={<Reserve />} />
+    //   </Routes>    
+    // </BrowserRouter>
+    // </Provider>
+    // </QueryClientProvider>
+    // </CookiesProvider>
+  );
+}
+
+export default App;
