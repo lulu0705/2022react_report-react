c9a4e8ad0813c5680672dac84f0296d74a23d205
lulu0705 <86127529+lulu0705@users.noreply.github.com>
2022-12-28 01:57:59 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
-import { CookiesProvider } from 'react-cookie';
+// import { CookiesProvider } from 'react-cookie';
 import { Provider } from "react-redux";
 import store from './redux/store';
 
@@ -27,28 +27,37 @@ const queryClient = new QueryClient()
 
 function App() {
   return (
-    <CookiesProvider>
     <QueryClientProvider client={queryClient}>
-    <Provider store={store}>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Index" element={<Index />} />
-
-        {/* <Route path="products"> */}
-          <Route path="products/category/:categoryName" element={<Index />} />
-          <Route path="/product/:productId" element={<Product />} />
-        {/* </Route> */}
-
+        <Route path="products/category/:categoryName" element={<Index />} />
+        <Route path="/product/:productId" element={<Product />} />
         <Route path="/ResultPage" element={<ResultPage />} />
         <Route path="/Feedback" element={<Feedback />} />
         <Route path="/Reserve/:productId" element={<Reserve />} />
-
       </Routes>    
     </BrowserRouter>
-    </Provider>
     </QueryClientProvider>
-    </CookiesProvider>
+
+    // <CookiesProvider>
+    // <QueryClientProvider client={queryClient}>
+    // <Provider store={store}>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/Index" element={<Index />} />
+    //     <Route path="products/category/:categoryName" element={<Index />} />
+    //     <Route path="/product/:productId" element={<Product />} />
+    //     <Route path="/ResultPage" element={<ResultPage />} />
+    //     <Route path="/Feedback" element={<Feedback />} />
+    //     <Route path="/Reserve/:productId" element={<Reserve />} />
+    //   </Routes>    
+    // </BrowserRouter>
+    // </Provider>
+    // </QueryClientProvider>
+    // </CookiesProvider>
   );
 }
 
